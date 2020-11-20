2c3816a6d4d04ec5dd17f14846fffeef7973211a
donngchao <marquisdong@tencent.com>
2020-11-20 19:10:22 +0800

    exercise2.1 and 2.2

---
+++ chapter_02/ex2.1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author:developer
+@contact:
+@software:
+@file: ex2.1.py
+@time: 2020/11/20 7:05 下午
+@desc:
+'''
+
+
+signal = "Signal from linux kernel..."
+print(signal)
---
+++ chapter_02/ex2.2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: developer
+@contact:
+@software:
+@file: ex2.2.py
+@time: 2020/11/20 7:07 下午
+@desc:
+'''
+
+short_message = "Keep calm..."
+print(short_message)
+short_message = "Be careful..."
+print(short_message)
