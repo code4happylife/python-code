c21ef23662d44460592934f2a000ef261f610949
donngchao <1559273720@qq.com>
2020-11-22 22:12:20 +0800

    ex2.3

---
+++ chapter_02/ex2.3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: developer
+@contact:
+@software: onion test
+@file: ex2.3.py
+@time: 2020/11/22 10:09 下午
+@desc:
+'''
+
+user_name = "tom"
+say_hi = "hello, "+user_name+" ! wish you have a nice weekend..."
+print(say_hi)
